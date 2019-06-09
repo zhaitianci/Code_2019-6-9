0ff3b6b480d71ce0ad9c0f3ddb24b4f2291ee430
zhaitianci <1282176696@qq.com>
2019-06-09 11:10:04 +0800

    第2次改动

--- app/src/main/java/com/example/dome201969/MainActivity.java
+++ app/src/main/java/com/example/dome201969/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int i=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //1222爱的
+
     }
 }
